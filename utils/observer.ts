7f89b755426562ebd264dc761e310753a2a71d33
maoguotao <maoguotao@rayvision.com>
2020-04-16 14:14:04 +0800

    :sparkles: update

---
+++ utils/observer.ts
@@ -0,0 +1,30 @@
+
+interface Events {
+  [propname: string]: any
+}
+
+const defaultFunction = () => {}
+class Observer {
+  events: Events
+  constructor () {
+    this.events = {}
+  }
+  subscribe (eventName: string, fn = defaultFunction) {
+    this.events[eventName] = fn
+  }
+  publish (eventName) {
+    if (this.events[eventName]) {
+      return this.events.eventName
+    }
+    return false
+  }
+  unsubscribe (eventName: string) {
+    const events = this.events[eventName]
+    if (events) {
+      Reflect.deleteProperty(this.events, eventName)
+    }
+  }
+}
+
+export default new Observer()
+
