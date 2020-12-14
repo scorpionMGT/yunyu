2eb3a2f3db258341e155426fcc97a2e31709949b
maoguotao <1142656486@qq.com>
2020-12-14 23:46:41 +0800

    fix: fix添加 server.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,13 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   console.log('proxy', app)
-  app.use(proxy('/api', {
-    target: 'http://127.0.0.1:7001',
-    changeOrigin: true,
-    onProxyReq: (proxyReq, req, res) => {
-      console.log('proxyReq path', proxyReq.path)
-    }
-   }))
-};
+  app.use(
+    proxy('/api', {
+      target: 'https://api.cloudcat.show',
+      changeOrigin: true,
+      onProxyReq: (proxyReq, req, res) => {
+        console.log('proxyReq path', proxyReq.path)
+      },
+    })
+  )
+}
