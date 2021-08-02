b4afdfe90ab8535e0336cb65572f6e05825a0c25
毛国涛 <1142656486@qq.com>
2021-08-02 21:28:31 +0800

    perf: 乾坤

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ import zhCN from 'antd/es/locale/zh_CN'
 import { ConfigProvider } from 'antd'
 import { Provider } from 'react-redux'
 import store from './store'
+import { qiankunFunc } from './qiankun'
 
+qiankunFunc()
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <Provider store={store}>
         <ConfigProvider locale={zhCN}>
-          <Router>
+          <Router basename={window.__POWERED_BY_QIANKUN__ ? '/app-react' : '/'}>
             <GlobalStyle />
             <AppMain />
           </Router>
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'antd/dist/antd.css'
+import './public-path'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+function render(props: any) {
+  const { container } = props
+  ReactDOM.render(<App />, container ? container.querySelector('#root') : document.querySelector('#root'))
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({})
+}
+
+export async function bootstrap() {
+  console.log('[react16] react app bootstraped')
+}
+
+export async function mount(props: any) {
+  console.log('[react16] props from main framework', props)
+  render(props)
+}
+
+export async function unmount(props: any) {
+  const { container } = props
+  ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'))
+}
