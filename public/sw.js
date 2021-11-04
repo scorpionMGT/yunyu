9c3c7cb511d3f90c02809ff4d97097e261ac43c3
maoguotao <v_maoguotao@fowo.com>
2021-11-04 14:52:20 +0800

    perf: 更新

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,6 @@
 // 我们可以使用self来表示全局作用域，self, 没有窗体这个概念 window === self
 self.addEventListener('install', event => {
     let CACHE_NAME ='yunyu-cache' 
-    console.log('安装install ', self);
     let urlsToCache = [
         '/static/js/0.chunk.js',
         '/static/js/1.chunk.js',
@@ -14,20 +13,18 @@ self.addEventListener('install', event => {
         '/home'
     ]
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache =>  {
+        caches.open(CACHE_NAME).then(cache => {
             console.log('cache', cache)
-            cache.addAll(urlsToCache)
+            return cache.addAll(urlsToCache)
         })
     )
 })
 
 self.addEventListener('activate', event => {
-    console.log('激活activate', event);
     // https://developer.mozilla.org/zh-CN/docs/Web/API/ExtendableEvent/waitUntil
     event.waitUntil(
         // https://developer.mozilla.org/zh-CN/docs/Web/API/Clients/matchAll
         self.clients.matchAll().then(function(clientList) {
-            console.log('clientList', self.clients, clientList);
             for (let i = 0 ; i < clients.length; i++) {
                 if (clientList[i].url === 'index.html') {
                     self.clients.openWindow(clientList[i]);
@@ -53,7 +50,6 @@ self.addEventListener('fetch', function(event) {
         
         caches.open('yunyu-cache').then(function (cache) {
         //   cache.put(event.request, responseClone);
-        console.log('cache', cache)
         });
         return response;
       }).catch(function () {
