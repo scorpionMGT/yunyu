aef1ee88b8d6285f91eb20489ef3c4e0b4f17844
maoguotao <v_guotao.mao@fowo.com>
2021-11-08 16:16:08 +0800

    perf: 更新pwa

--- public/sw.js
+++ public/sw.js
@@ -1,60 +1,153 @@
-// 我们可以使用self来表示全局作用域，self, 没有窗体这个概念 window === self
+// configuration
+`use strict`;
+
+const
+  version = '1.0.0',
+  CACHE = version + '::PWAsite',
+  offlineURL = '/offline/',
+  installFilesEssential = [
+    '/',
+    '/manifest.json',
+    '/css/styles.css',
+    '/js/main.js',
+    '/js/offlinepage.js',
+  ].concat(offlineURL),
+  installFilesDesirable = [
+    '/favicon.ico',
+  ];
+
+// install static assets
+function installStaticFiles() {
+
+  return caches.open(CACHE)
+    .then(cache => {
+
+      // cache desirable files
+      cache.addAll(installFilesDesirable);
+
+      // cache essential files
+      return cache.addAll(installFilesEssential);
+
+    });
+
+}
+
+// clear old caches
+function clearOldCaches() {
+
+  return caches.keys()
+    .then(keylist => {
+
+      return Promise.all(
+        keylist
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      );
+
+    });
+
+}
+
+// application installation
 self.addEventListener('install', event => {
-    let CACHE_NAME ='yunyu-cache' 
-    let urlsToCache = [
-        '/static/js/0.chunk.js',
-        '/static/js/1.chunk.js',
-        '/static/js/bundle.js',
-        '/static/js/main.chunk.js',
-        '/manifest.json',
-        '/favicon.ico',
-        '/index.html',
-        '/sockjs-node/info',
-        '/home'
-    ]
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('cache', cache)
-            return cache.addAll(urlsToCache)
-        })
-    )
-})
 
+  console.log('service worker: install');
+
+  // cache core files
+  event.waitUntil(
+    installStaticFiles()
+    .then(() => self.skipWaiting())
+  );
+
+});
+
+
+// application activated
 self.addEventListener('activate', event => {
-    // https://developer.mozilla.org/zh-CN/docs/Web/API/ExtendableEvent/waitUntil
-    event.waitUntil(
-        // https://developer.mozilla.org/zh-CN/docs/Web/API/Clients/matchAll
-        self.clients.matchAll().then(function(clientList) {
-            for (let i = 0 ; i < clients.length; i++) {
-                if (clientList[i].url === 'index.html') {
-                    self.clients.openWindow(clientList[i]);
-                    // or do something else involving the matching client
-                }
-            }
-        })
-    )
-})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-        
-        caches.open('yunyu-cache').then(function (cache) {
-        //   cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(function () {
-        return caches.match('/sw-test/gallery/myLittleVader.jpg');
-      });
-    }
-  }));
+  console.log('service worker: activate');
+
+	// delete old caches
+  event.waitUntil(
+    clearOldCaches()
+    .then(() => self.clients.claim())
+	);
+
+});
+
+
+// is image URL?
+let iExt = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].map(f => '.' + f);
+function isImage(url) {
+
+  return iExt.reduce((ret, ext) => ret || url.endsWith(ext), false);
+
+}
+
+
+// return offline asset
+function offlineAsset(url) {
+
+  if (isImage(url)) {
+
+    // return image
+    return new Response(
+      '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
+      { headers: {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-store'
+      }}
+    );
+
+  }
+  else {
+
+    // return page
+    return caches.match(offlineURL);
+
+  }
+
+}
+
+
+// application fetch network data
+self.addEventListener('fetch', event => {
+
+  // abandon non-GET requests
+  if (event.request.method !== 'GET') return;
+
+  let url = event.request.url;
+
+  event.respondWith(
+
+    caches.open(CACHE)
+      .then(cache => {
+
+        return cache.match(event.request)
+          .then(response => {
+
+            if (response) {
+              // return cached file
+              console.log('cache fetch: ' + url);
+              return response;
+            }
+
+            // make network request
+            return fetch(event.request)
+              .then(newreq => {
+
+                console.log('network fetch: ' + url);
+                if (newreq.ok) cache.put(event.request, newreq.clone());
+                return newreq;
+
+              })
+              // app is offline
+              .catch(() => offlineAsset(url));
+
+          });
+
+      })
+
+  );
+
 });
